915a0fcc37d379223c5425767434c3567a0589dc
Dimzmiks555 <53266916+Dimzmiks555@users.noreply.github.com>
2022-06-06 16:24:17 +0300

    meta

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import * as React from "react"
+import Helmet from "react-helmet"
 import { About } from "../components/About"
 import { Contact } from "../components/Contact"
 import { Footer } from "../components/Footer"
@@ -11,6 +12,12 @@ import '../styles/global.css'
 const IndexPage = () => {
   return (
       <main >
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>Ремонт бытовой техники по Воронежской области</title>
+          <meta name="keywords" content="Ремонт стиральных машин, Ремонт бытовой техники, Воронежская область, Лиски, Острогожск, Нововоронеж, Воронеж, Бобров, Ремонт бытовой техники по Воронежской области, мастер, мастер по ремонту бытовой техники, починить стиральную машину, стиральная машина" />
+          <meta name="description" content="Ремонт бытовой техники по Воронежской области. Консультация бесплатно. Ремонт в день обращения. Выезд мастера в г. Лиски, Острогожск, Нововоронеж, Воронеж, Бобров, и прилегающие города и села." />
+        </Helmet>
         <Header></Header>
         <About></About>
         <Review></Review>
