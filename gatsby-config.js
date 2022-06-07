85627641c530ae61f505d7cea627018151836ba3
Dimzmiks555 <53266916+Dimzmiks555@users.noreply.github.com>
2022-06-07 09:33:54 +0300

    favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,20 @@ module.exports = {
     title: `REMBITTEH`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-react-helmet", "gatsby-plugin-sitemap"]
+  plugins: [
+    "gatsby-plugin-react-helmet", 
+    "gatsby-plugin-sitemap", 
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'Ремонт бытовой техники',
+        short_name: 'Ремонт бытовой техники',
+        start_url: '/',
+        background_color: '#fff',
+        theme_color: '#fff',
+        display: 'standalone',
+        icon: 'src/images/LOGO.svg',
+      },
+    }
+  ]
 };
